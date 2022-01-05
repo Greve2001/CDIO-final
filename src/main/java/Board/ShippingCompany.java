6016af6998719b538ae9b2d21d9cc54e2db578c6
Emil Falck Hansen <falcken78@gmail.com>
2022-01-05 14:16:18 +0100

    Updated ShippingCompany

--- src/main/java/Board/ShippingCompany.java
+++ src/main/java/Board/ShippingCompany.java
@@ -1,17 +1,18 @@
 package Board;
-
 import java.awt.*;
+import Logic.Player;
 
 public class ShippingCompany extends Square{
-    private Color color;
+    private final String color;
     private final int[]  RENT;
     private final int PRICE;
     private boolean pledge = false;
     private Player owner = null;
 
-    public ShippingCompany(String name, int position, int[] rent, int price)
-    {
+
+    public ShippingCompany(String name, int position, int[] rent, int price, String color) {
         super(name, position);
+        this.color = color;
         this.RENT = rent;
         this.PRICE = price;
     }
