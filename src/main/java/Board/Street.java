c0168b1ccbde5040b378941bd4608518f1b2cc41
Christian Andersen <s133288@dtu.dk>
2022-01-05 14:14:49 +0100

    added houseprice, and finished the constructor

--- src/main/java/Board/Street.java
+++ src/main/java/Board/Street.java
@@ -1,16 +1,21 @@
 package Board;
 
+import Logic.Player;
+
 public class Street extends Square{
     private final String COLOR;
     private final int PRICE;
     private final int[] RENT;
+    private final int HOUSEPRICE;
 
     private Player owner = null;
     private boolean pledge = false;
 
-    public Street(String name, int position, int price, int[] rent){
+    public Street(String name, int position, String color, int price, int housePrice,int[] rent){
         super(name, position);
+        this.COLOR = color;
         this.PRICE = price;
+        this.HOUSEPRICE = housePrice;
         this.RENT = rent;
     }
 }
