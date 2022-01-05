44403945753bcd33ee9d079ce94bf1e1e43cbdbf
Emil Falck Hansen <falcken78@gmail.com>
2022-01-05 11:02:50 +0100

    Updated ShippingCompany

--- src/main/java/Board/ShippingCompany.java
+++ src/main/java/Board/ShippingCompany.java
@@ -1,4 +1,18 @@
 package Board;
 
+import java.awt.*;
+
 public class ShippingCompany extends Square{
+    private Color color;
+    private final int[]  RENT;
+    private final int PRICE;
+    private boolean pledge = false;
+    private Player owner = null;
+
+    public ShippingCompany(String name, int position, int[] rent, int price)
+    {
+        super(name, position);
+        this.RENT = rent;
+        this.PRICE = price;
+    }
 }
