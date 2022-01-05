72bbba3e26eaa26572d7ef705e6bf0833b39d6a6
Emil Falck Hansen <falcken78@gmail.com>
2022-01-05 10:38:54 +0100

    Updated Square class

--- src/main/java/Board/Square.java
+++ src/main/java/Board/Square.java
@@ -1,4 +1,20 @@
 package Board;
 
 public class Square {
+    private final String NAME;
+    private final int POSITION;
+
+    public Square (String name, int position)
+    {
+        this.NAME = name;
+        this.POSITION = position;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public int getPOSITION() {
+        return POSITION;
+    }
 }
