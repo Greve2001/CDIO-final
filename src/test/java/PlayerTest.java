18d3d35ad832f172b79f3407f5ac15633881cfc7
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-01-05 11:01:17 +0100

    Added Player class and PlayerTest

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    String name;
+    int balance;
+    int position;
+    Player p;
+
+    @BeforeEach
+    public void setup(){
+        //Setup
+        name = "TestPlayer";
+        balance = 0;
+        position = 0;
+        p = new Player(name, balance, position);
+    }
+
+    @Test
+    public void setAndGetBalanceTest(){
+        // Action
+        int playersBalanceBefore = p.getBalance();
+        p.setBalance(20);
+        int playersBalanceAfter = p.getBalance();
+
+        int expectedBalanceBefore = 0;
+        int expectedBalanceAfter = 20;
+
+        // Assertions
+        assertEquals(expectedBalanceBefore, playersBalanceBefore);
+        assertEquals(expectedBalanceAfter, playersBalanceAfter);
+    }
+
+    @Test
+    public void setAndGetPositionTest(){
+        // Action
+        int playersPositionBefore = p.getPosition();
+        p.setPosition(3);
+        int playersPositionAfter = p.getPosition();
+
+        int expectedPositionBefore = 0;
+        int expectedPositionAfter = 3;
+
+        // Assertions
+        assertEquals(expectedPositionBefore, playersPositionBefore);
+        assertEquals(expectedPositionAfter, playersPositionAfter);
+    }
+
+
+    @Test
+    public void getNameTest(){
+        String playersName = p.getName();
+
+        assertTrue(name.equals(playersName));
+    }
+}
