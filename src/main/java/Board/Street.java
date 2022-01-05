37d9686a64a72cec95d919d5e535fb687d5fb0da
Christian Andersen <s133288@dtu.dk>
2022-01-05 11:02:42 +0100

    creating constructor

--- src/main/java/Board/Street.java
+++ src/main/java/Board/Street.java
@@ -2,11 +2,15 @@ package Board;
 
 public class Street extends Square{
     private final String COLOR;
-    private final int[] PRICE;
+    private final int PRICE;
+    private final int[] RENT;
+
     private Player owner = null;
+    private boolean pledge = false;
 
-    public Street(String name, int position){
+    public Street(String name, int position, int price, int[] rent){
         super(name, position);
-
+        this.PRICE = price;
+        this.RENT = rent;
     }
 }
