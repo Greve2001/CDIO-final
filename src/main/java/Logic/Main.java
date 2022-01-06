49357ba5237f9e10dd0f492ca06e23d797a7633e
Christian Andersen <s133288@dtu.dk>
2022-01-06 11:19:52 +0100

    gave it the main logic

--- src/main/java/Logic/Main.java
+++ src/main/java/Logic/Main.java
@@ -2,6 +2,8 @@ package Logic;
 
 public class Main {
     public static void main(String[] args) {
-
+        GameController matador = new GameController();
+        matador.setupGame();
+        matador.playGame();
     }
 }
