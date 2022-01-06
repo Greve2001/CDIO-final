e18d43c99c58f6a20ea9245f3ba59bbf26a54568
Emil Falck Hansen <falcken78@gmail.com>
2022-01-06 10:56:23 +0100

    Added methods hasMonopoly, movePlayer and setPlayerPosition.

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -2,9 +2,11 @@ package Board;
 
 import Logic.Player;
 import Utilities.CSVReader;
+import org.jetbrains.annotations.Debug;
 
 public class Board {
     private final Square[] ALL_SQUARES;
+
 
     public Board(){
         CSVReader reader;
@@ -81,9 +83,33 @@ public class Board {
         }
         return result;
     }
+    public boolean hasMonopoly() {
+       return false;
+    }
 
-    public void payStartBonus(Player currentPlayer)
-    {
+
+    public void movePlayer(Player player, int spacesToMove) {
+        Board board = new Board();
+        int BOARD_SIZE = board.getALL_SQUARES().length;
+        int endPos = player.getPosition() + spacesToMove;
+        int newPos;
+        if (spacesToMove == Math.abs(spacesToMove)){
+            if (endPos > BOARD_SIZE)
+                newPos = endPos - BOARD_SIZE;
+            else{
+                if (endPos < 0)
+                    newPos = endPos + BOARD_SIZE;
+                else
+                    newPos = endPos;
+            }
+            player.setPosition(newPos);
+        }
+    }
+    public void setPlayerPosition(Player player, int endPos){
+        player.setPosition(endPos);
+    }
+
+    public void payStartBonus(Player currentPlayer) {
 
     }
     public Square[] getALL_SQUARES() {
