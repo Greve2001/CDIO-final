a21a82c5b2ffd3f36cfd1e677002cb8118277f43
Christian Andersen <s133288@dtu.dk>
2022-01-06 09:21:21 +0100

    began setup of test

---
+++ src/test/java/Board/BoardTest.java
@@ -0,0 +1,12 @@
+package Board;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
