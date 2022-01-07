31d7f92c96405535ece10eee7c49fddbb1a1ec09
mikael <mikael.fangel@pm.me>
2022-01-07 14:11:01 +0100

    instantiated language at the top level

--- src/main/java/Logic/Main.java
+++ src/main/java/Logic/Main.java
@@ -1,7 +1,10 @@
 package Logic;
 
+import Utilities.Language;
+
 public class Main {
     public static void main(String[] args) {
+        Language.getInstance();
         GameController matador = new GameController();
         matador.setupGame();
         matador.playGame();
