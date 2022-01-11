a4cc89b5401d9d38d32b8d06279f78436be1cf46
Emil Falck Hansen <falcken78@gmail.com>
2022-01-11 11:30:46 +0100

    Made GameControlerTest

---
+++ src/test/java/Logic/GameControllerTest.java
@@ -0,0 +1,48 @@
+package Logic;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameControllerTest {
+    GameController gameController;
+
+    @BeforeEach
+    void setUp(){
+        gameController = new GameController();
+    }
+
+    @Test
+    void testAllowedNumberOfPlayers()
+    {
+        int[] numOfPlayers = {0, 1, 2, 3, 4, 5, 6};
+        int[] expectedNumPlayers = {0, 0, 0, 3 , 4, 5, 6};
+
+        for (int i = 0; i < numOfPlayers.length; i++){
+            String[] playerNames = new String[numOfPlayers[i]];
+            for (int j = 0; j < numOfPlayers[i]; j++){
+                playerNames[j] = Integer.toString(j);
+            }
+            gameController.setupPlayers(playerNames);
+
+            int actualNumOfPlayers = gameController.getPlayers().length;
+
+            assertEquals(expectedNumPlayers[i], actualNumOfPlayers);
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
