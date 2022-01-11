e44bc6a13ead865d0d8f116c3ffe18ff4454d4c0
mikael <mikael.fangel@pm.me>
2022-01-11 11:20:38 +0100

    changed how the test got the csv

--- src/test/java/Logic/ActionHandlerTest.java
+++ src/test/java/Logic/ActionHandlerTest.java
@@ -9,31 +9,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActionHandlerTest {
-    Board board = new Board();
-    ActionHandler actionHandler = new ActionHandler(board);
-    Square[] squares = new Square[10];
+    Board board;
+    ActionHandler actionHandler;
     Player[] players = new Player[3];
+    Square[] squares;
 
 
     @BeforeEach
     void setUp() {
         System.setProperty("user.language", "da");
         Language.getInstance();
+        board = new Board();
+        squares = board.getALL_SQUARES();
+        actionHandler = new ActionHandler(board);
         new GUIController(board);
         GUIController.setTesting(true);
-
-        int[] rents = {1000, 2000, 3000, 4000, 5000, 6000};
-
-        squares[0] = new Start("Start", 1, 4000);
-        squares[1] = new Street("Street", 2, "Street", "Yellow", rents, 2000, 2000);
-        squares[2] = new Chance("Chance", 3);
-        squares[3] = new GoToPrison("GoToPrison", 4);
-        squares[4] = new Ferry("Ferry", 5, "Street", "Yellow", rents, 2000);
-        squares[5] = new Brewery("Brewery", 6, "Street", "Yellow", rents, 2000);
-        squares[6] = new Tax("Tax", 7, 2000);
-        squares[7] = new IncomeTax("IncomeTax", 8, 4000, 10);
-        squares[8] = new Refugee("Refuge", 9);
-        squares[9] = new Prison("Prison", 10);
 
         players[0] = new Player("Player 1", 30000, 1);
         players[1] = new Player("Player 2", 30000, 1);
@@ -44,7 +34,7 @@ class ActionHandlerTest {
     void streetActionBuyingDeed() {
         actionHandler.squareAction(players[0], squares[1], 10);
 
-        int expectedBalance = 28000;
+        int expectedBalance = 28800;
         int actualBalance = players[0].getBalance();
         assertEquals(expectedBalance, actualBalance);
 
@@ -58,7 +48,7 @@ class ActionHandlerTest {
         squares[1].setOwner(players[0]);
         actionHandler.squareAction(players[1], squares[1], 10);
 
-        int expectedBalance = 29000;
+        int expectedBalance = 29950;
         int actualBalance = players[1].getBalance();
         assertEquals(expectedBalance, actualBalance);
     }
