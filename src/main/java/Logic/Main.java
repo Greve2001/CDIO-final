016974eb89bc51a918200a47a808657be20e27c2
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-01-15 21:32:58 +0100

    merge conflict with main

--- src/main/java/Logic/Main.java
+++ src/main/java/Logic/Main.java
@@ -7,20 +7,6 @@ public class Main {
         Language.getInstance();
         GameController matador = new GameController();
         matador.setupGame();
-
-        // FOR TESTING BUG REMOVE
-        Player[] players = matador.getPlayers();
-        players[0].setBalance(500000);
-        players[1].setBalance(500000);
-        players[2].setBalance(500000);
-
-        Board board = matador.getBoard();
-        Square[] squares = board.getALL_SQUARES();
-
-        squares[1].setOwner(players[0]);
-        squares[3].setOwner(players[0]);
-        // END OF TESTING CONDITIONS
-
         matador.playGame();
     }
 }
